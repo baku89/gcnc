7c1b3d32c59847e4b065301a837d28d62f04a4e4
Baku Hashimoto <mail@baku89.com>
2024-12-22 13:52:34 +0900

    Support OSC output

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 import fs from 'node:fs'
 import readline from 'node:readline'
 
+import OSC from 'osc-js'
 import yargs from 'yargs'
 import {hideBin} from 'yargs/helpers'
 
@@ -27,9 +28,70 @@ const argv = await yargs(hideBin(process.argv))
 		type: 'number',
 		description: 'Line number to start from',
 	})
+	.option('osc-port', {
+		default: 8080,
+		type: 'number',
+		description: 'OSC port to send to',
+	})
 	.help().argv
 
-const device = new SerialCNCDevice(argv.port, {checkStatusInterval: 100})
+const osc = new OSC({
+	plugin: new OSC.DatagramPlugin({
+		// @ts-expect-error: osc-js is not typed
+		send: {port: argv.oscPort},
+	}),
+})
+
+console.log(`Launching OSC server on port ${argv.oscPort}`)
+
+osc.on('open', () => {
+	console.log(`OSC server is running on port ${argv.oscPort}`)
+})
+
+osc.open()
+
+const device = new SerialCNCDevice(argv.port, {checkStatusInterval: 50})
+
+device.on('status', status => {
+	if (!osc) return
+
+	// 軸位置の送信
+	const axes = [
+		status.position.x,
+		status.position.y ?? 0,
+		status.position.z ?? 0,
+		status.position.a ?? 0,
+		status.position.b ?? 0,
+		status.position.c ?? 0,
+	]
+
+	osc.send(new OSC.Message('/gcnc/report/axes', ...axes))
+
+	// ステータスの送信
+	osc.send(new OSC.Message('/gcnc/report/status', status.state))
+
+	// 送り速度の送信
+	osc.send(new OSC.Message('/gcnc/report/feedRate', status.feedRate ?? 0))
+})
+
+// Gコード送信時のハンドラ
+device.on('sent', gcode => {
+	if (!osc) return
+
+	// 送信したコマンドの通知
+	const axes = [
+		gcode.x ?? 0,
+		gcode.y ?? 0,
+		gcode.z ?? 0,
+		gcode.a ?? 0,
+		gcode.b ?? 0,
+		gcode.c ?? 0,
+	]
+
+	osc.send(new OSC.Message('/gcnc/sent/axes', ...axes))
+	osc.send(new OSC.Message('/gcnc/sent/command', gcode.command))
+	osc.send(new OSC.Message('/gcnc/sent/feedRate', gcode.feedRate ?? 0))
+})
 
 await device.open().catch(err => {
 	const msg = err instanceof Error ? err.message : 'Unknown error'
