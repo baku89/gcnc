a93790d4538b6c70e9003f69b98f667c1d14e7f9
Baku Hashimoto <mail@baku89.com>
2025-01-03 01:02:45 +0900

    Support ^X to reset the device

--- src/cli/startRepl.ts
+++ src/cli/startRepl.ts
@@ -1,20 +1,36 @@
 import {createInterface} from 'node:readline'
 
-import fromCallback from 'p-from-callback'
-
 import {CNCDevice} from '../CNCDevice.js'
+import {withResolvers} from '../util.js'
 
+// https://qiita.com/shellyln/items/9f6c85afee2f7a5773d7
 export async function startRepl(device: CNCDevice) {
+	const {promise, resolve} = withResolvers()
+
 	const rl = createInterface({
 		input: process.stdin,
 		output: process.stdout,
+		prompt: '>',
 	})
 
-	while (true) {
-		const command = await fromCallback<string>(cb =>
-			rl.question('>', answer => cb(null, answer))
-		)
-		const res = await device.send(command).catch(err => err)
+	rl.prompt()
+
+	// Detect Ctrl-X (Reset)
+	process.stdin.on('keypress', async (_, key) => {
+		if (key.ctrl && key.name === 'x') {
+			const res = await device.send('\x18').catch(err => err)
+			console.log(res)
+			rl.prompt()
+		}
+	})
+
+	rl.on('line', async input => {
+		const res = await device.send(input).catch(err => err)
 		console.log(res)
-	}
+		rl.prompt()
+	}).on('close', () => {
+		resolve()
+	})
+
+	return promise
 }
